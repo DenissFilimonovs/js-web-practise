aded56f9c8dc1e3ee9971cf9282296654cfc62ee
Deniss Filimonovs <104933477+Driwavij@users.noreply.github.com>
2022-06-26 09:02:47 +0300

    Update SQL.js

--- SQL.js
+++ SQL.js
@@ -8,3 +8,9 @@ UNION
 SELECT class
 FROM Ships LEFT JOIN Outcomes ON Outcomes.ship = ships.name
 WHERE result = 'sunk'
+
+
+
+//Given a year, return the century it is in.
+SELECT (yr+99)/100 AS century 
+FROM years
