9e376f72094a3c3b746f8ad68cc477051cd7ea00
Deniss Filimonovs <104933477+Driwavij@users.noreply.github.com>
2022-06-26 10:50:42 +0300

    Update Practise.js

--- Practise.js
+++ Practise.js
@@ -600,3 +600,11 @@ function func(arr){
     }
 return result // [1, 2, 7, 8, 3, 4, 5, 6, 7]
 }
+
+//Дан массив с элементами 1, 2, 3, 4, 5. С помощью цикла for-of найдите сумму элементов этого массива.
+let arr = [1,2,3,4,5]
+let sum = 0
+for(let elem of arr){
+    sum +=elem
+    console.log(sum)
+}
