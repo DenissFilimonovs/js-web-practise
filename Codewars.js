70140f3e2d27b93db26a503f3bb2778d84fbc922
Deniss Filimonovs <104933477+DenissFilimonovs@users.noreply.github.com>
2022-07-12 18:39:08 +0300

    Update Codewars.js

--- Codewars.js
+++ Codewars.js
@@ -104,3 +104,8 @@ function mergeArrays(arr1, arr2) {
      return Array.from(new Set(arr1.concat(arr2))).sort((a,b) => a - b);   
     }
 }
+
+//CSV representation of array.js
+function toCsvText(array) {
+   return array.join('\n')
+}
